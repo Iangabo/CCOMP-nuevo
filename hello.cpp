e4803199144289c94d02000bec29a1426bd69f46
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 08:57:29 -0500

    suma

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int x=6,y=5;
+ int suma=x+y;
+ int resta=x-y;
+ int mult=x*y;
+
+ cout << "La suma de los dos numeros es: "<<suma<<endl;
+ cout << "La resta de los dos numeros es: "<<resta<<endl;
+ cout << "La multiplicacion de los dos numeros es: "<<mult<<endl;
+
+ return 0;
+}
