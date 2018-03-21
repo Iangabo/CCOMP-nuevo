e88104330c3951f42ff5a797eded336ead518d8f
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-03-21 09:42:25 -0500

    suma

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
+/*
 int main()
 {
- int x=6,y=5;
+ int x,y;
+ cout<<"Ingrese un valor para x: ";
+ cin>>x;
+ cout<<"Ingrese un valor para y: ";
+ cin>>y;
  int suma=x+y;
  int resta=x-y;
  int mult=x*y;
@@ -14,4 +19,19 @@ int main()
  cout << "La division de los dos numeros es: "<<div<<endl;
 
  return 0;
+}
+*/
+
+int main()
+{
+    cout<<"Area de un circulo: "<<endl;
+    float y;
+    cout<<"ingrese el radio del circulo: ";
+    cin>>y;
+
+    float area=3.1416*(y*y);
+    cout<<"El Area del ciculo es: "<<area<<endl;
+
+    return 0;
+
 }
