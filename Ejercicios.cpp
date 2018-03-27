f74e61ffb86e8fcba696cc4272a24af76f4f2616
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-27 09:13:26 -0500

    Ian

---
+++ Ejercicios.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+/*
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    string a= "**********          ***            *";
+    string d= "*        *        *     *         * *";
+    string b= "*        *       *       *       *   *";
+    string c= "*        *       *       *      *     *";
+    string e= "*        *       *       *     *       *";
+    string f= "*        *        *     *     *         *";
+    string g= "**********          ***      *************";
+
+    cout <<a<<endl;
+    cout <<d<<endl;
+    cout <<b<<endl;
+    cout <<c<<endl;
+    cout <<e<<endl;
+    cout <<f<<endl;
+    cout <<g<<endl;
+
+return 0;
+}
+*/
+int main()
+{
+   int p=2;
+   int r=3;
+   int q=4;
+   int w=5;
+   int x=6;
+   int y=7;
+
+   int z=(p*r)%q+w/x-y;
+
+   cout<<z<<endl;
+
+   return 0;
+}
