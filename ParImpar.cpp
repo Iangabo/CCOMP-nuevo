48a9dbdd23fabe0bbe9b77d10256621e65227d3b
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-27 09:47:50 -0500

    Ian

---
+++ ParImpar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int x;
+    cout<<"Ingrese un valor para x: "<<endl;
+    cin>>x;
+
+    if (x%2){
+    cout<<"Es impar"<<endl;
+
+    }
+    else{
+    cout<<"Es par"<<endl;
+    }
+
+    return 0;
+ }
