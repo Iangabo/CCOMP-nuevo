32e83dbfefe5b8f084fc0593166955c7c6158534
Ian Camero <ian.camero@ucsp.edu.pe>
2018-04-02 21:09:01 -0500

    Ian

---
+++ Trabajo.cpp
@@ -0,0 +1,190 @@
+#include <iostream>
+
+using namespace std;
+/*
+
+//Problema 1:
+int main()
+{
+    int a;
+    cout<<"Ingrese su edad: "<<endl;
+    cin>>a;
+
+    if (a<18){
+        cout<<"Usted es menor de edad"<<endl;
+
+    }
+    else {
+        cout<<"Usted es mayor de edad"<<endl;
+    }
+    return 0;
+}
+
+//Problema 2
+int main()
+{
+    int n;
+    int a=0;
+    cout<<"Ingrese un numero: "<<endl;
+    cin>>n;
+
+
+    while (a!=n)
+        {
+            cout<<a<<",";
+            a++;
+            if (a==n){
+                cout<<a;
+                break;
+            }
+    return 0;
+}
+
+
+//Problema 3
+int main (){
+
+ int a,b,c,d,e;
+ cout<<"Ingrese un valor para a: "<<endl;
+ cin>>a;
+ cout<<"Ingrese un valor para b:"<<endl;
+ cin>>b;
+ cout<<"Ingrese un valor para c:"<<endl;
+ cin>>c;
+ cout<<"Ingrese un valor para d:"<<endl;
+ cin>>d;
+ cout<<"Ingrese el un valor para e:"<<endl;
+ cin>>e;
+ int prom=(a+b+c+d+e)/5;
+ int mayor=a;
+ int menor=b;
+ if(b>mayor){
+    mayor=b;
+    }
+ if (c>mayor){
+    mayor=c;
+ }
+  if (d>mayor){
+    mayor=d;
+ }
+  if (e>mayor){
+    mayor=e;
+
+
+ if (menor>a){
+    menor=a;
+ }
+ if (menor>c){
+    menor=c;
+ }
+ if (menor>d){
+    menor=d;
+    }
+ if (menor>e){
+    menor=e;
+   }
+
+ cout<<"El mayor es "<<mayor<<endl;
+ cout <<"El menor es"<<menor<<endl;
+ cout <<"El promedio es: "<<prom<<endl;
+
+ return 0;
+ }
+}
+
+
+//Problema 4
+
+int main()
+{
+    int a,b;
+    cout<<"Ingrese un valor para a: "<<endl;
+    cin>>a;
+    cout<<"Ingrese un valor para b: "<<endl;
+    cin>>b;
+
+    if(a%2==0){
+        cout<<"El primero es multiplo de dos"<<endl;
+    }
+    else{
+        cout<<"El primero no es multiplo de dos"<<endl;
+    }
+
+     if(b%2==0){
+        cout<<"El segundo es multiplo de dos"<<endl;
+    }
+    else{
+        cout<<"El segundo no es multiplo de dos"<<endl;
+    }
+
+    if(a%b==0){
+        cout<<"El primero es multiplo del segundo"<<endl;
+    }
+    else{
+        cout<<"El primero no es multiplo del segundo"<<endl;
+    }
+    if((a*a)==b){
+        cout<<"El primero elevado al cuadrado es igual al segundo"<<endl;
+    }
+
+}
+
+
+
+//Problema 5
+
+int main()
+{
+    cout <<"Ingrese un numero"<< endl ;
+    int x;
+    cin >>x;
+        if(x%2!=0 && x%3!=0 && x%5!=0 && x%7!=0 )
+            cout <<"Si es primo" << endl ;
+        else
+            cout <<"No es primo" << endl ;
+    return 0;
+}
+
+
+
+//Problema 6
+//Problema 7
+//Problema 8
+
+
+//Problema 9
+int main(){
+      int a;
+      cout<<"Ingresa anio"<<endl;
+      cin>>a;
+     if(a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)){
+         cout<<"El anio "<<a<<"es bisiesto ";
+     }else{
+         cout<<"El anio "<<a<<" no es bisiesto ";
+     }
+      return 0;
+
+}
+
+
+
+//Problema 10
+*/
+
+//Problema 10
+int main()
+{
+  int num;
+  cout << "Ingrese un numero: " ;
+  cin >> num;
+  cout << "El factorial  " << num << " es: ";
+  int acumula=1;
+  for(; num > 0; acumula *= num--);
+  cout << acumula;
+  return 0;
+
+}
+
+
+
+
