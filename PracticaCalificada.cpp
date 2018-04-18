46cbdd33ac0044388034766e17b7620da4789e1c
Iangabo <31105000+Iangabo@users.noreply.github.com>
2018-04-18 00:32:09 -0500

    Update PracticaCalificada.cpp

--- PracticaCalificada.cpp
+++ PracticaCalificada.cpp
@@ -24,7 +24,16 @@ bool esPerfecto(int x)
 
 int main()
 {
-    cout <<esPerfecto(6);
+    for(int i=5; i<=1000;i++){
+        if(esPerfecto(i)){
+            cout<<i<<" : ";
+            for (int j=1;j<i;j++){
+                if(i%j==0){
+                    cout<<j<<",";
+                }
+            }
+        }
+    }
 }
 
 */
