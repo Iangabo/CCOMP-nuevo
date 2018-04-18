c47a60e4c4f67d309f73f80929a7c1ef06bb43bc
Ian Camero <ian.camero@ucsp.edu.pe>
2018-04-18 00:24:57 -0500

    Ian

---
+++ PracticaCalificada.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+/*
+//Pregunta 1
+
+bool esPerfecto(int x)
+{
+    int contador=0;
+    for(int i=1; i<x ;i++){
+        if(x%i==0){
+            contador=contador+1;
+        }
+    }
+    if(contador==x){
+        return true;
+
+    }
+    else{
+        return false;
+
+    }
+}
+
+int main()
+{
+    cout <<esPerfecto(6);
+}
+
+*/
+//Pregunts 2
+/*
+bool esPrimo(int x)
+{
+    int contador=0;
+    for(int i=1; i<=x ; i++){
+        if(x%i==0){
+            contador++;
+        }
+    }
+    if (contador==2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int main()
+{
+    int x;
+    cout<<"Ingrese un numero: "<<endl;
+    cin>>x;
+    for(int i=1;i<=x;i++){
+        if(esPrimo(i)){
+            cout<<i<<endl;
+        }
+    }
+}
+
+*/
+//Pregunta 3
+int potencia(int base, int exponente)
+{
+    if(exponente==0){
+        return 1;
+    }
+    else{
+        base=base*potencia(base, exponente-1);
+    }
+    return base;
+}
+
+int main()
+{
+    cout<<potencia(3,3);
+}
